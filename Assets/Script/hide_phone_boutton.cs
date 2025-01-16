41ffb97d1d915bc3cdfddd366edf3487f93357b3
Lucas ANDRES <lucas.andres@epitech.eu>
2025-01-16 09:38:49 +0100

    feat add ring song for phone on every end scene

--- Assets/Script/hide_phone_boutton.cs
+++ Assets/Script/hide_phone_boutton.cs
@@ -1,11 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class hide_phone_boutton : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    AudioSource audio;
+
     void Start()
     {
-        
+        audio = GetComponent<AudioSource>();
+        audio.Play();
+        StartCoroutine(ring());
+    }
+
+    IEnumerator ring()
+    {
+        yield return new WaitForSeconds(3.0f);
+        audio.Play();
+        StartCoroutine(ring());
     }
 
     // Update is called once per frame
@@ -13,8 +27,10 @@ public class hide_phone_boutton : MonoBehaviour
     {
 
     }
+
     public void hide_boutton()
     {
         gameObject.SetActive(false);
+        audio.Stop();
     }
 }
